d2a6df99eef5bfa5ede60ef696c1718150e16b7c
VonRiddarn <vonriddarn@gmail.com>
2024-12-10 05:36:33 +0100

    feat: made buttons regenerate the page and show wether you answered correctly in console

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,27 @@ import utilities from "./utilities";
 const query = document.getElementById("current-query") as HTMLElement;
 const answers = document.getElementById("answers") as HTMLElement;
 
-let currentQuery = utilities.getRandomQuery(1, 12);
-let solutionList = utilities.getAdjacentSolutions(currentQuery);
-solutionList.push(currentQuery.solution);
+// TODO: Tidy this up big time. This does not adhere to separation of concerns.
+// It only serves as a test / proof of concept
+function refresh()
+{
+	let currentQuery = utilities.getRandomQuery(1, 12);
+	let solutionList = utilities.getAdjacentSolutions(currentQuery);
 
-query.innerHTML = `${currentQuery.a} x ${currentQuery.b}`;
+	solutionList.push(currentQuery.solution);
+	solutionList = utilities.fisherYatesShuffle(solutionList);
 
-solutionList.forEach(element => {
-	let b = answers.appendChild(document.createElement("button") as HTMLButtonElement);
-	b.innerHTML = element.toString();
-});
+	query.innerHTML = `${currentQuery.a} x ${currentQuery.b}`;
+	answers.innerHTML = "";
+
+	solutionList.forEach(element => {
+		let b = answers.appendChild(document.createElement("button") as HTMLButtonElement);
+		b.innerHTML = element.toString();
+		b.addEventListener('click', (e) => {
+			console.log(`${element} ${element === currentQuery.solution ? "=" : "!="} ${currentQuery.solution}`);
+			refresh();
+		});
+	});
+}
+
+refresh();
