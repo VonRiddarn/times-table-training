96132bdf8415eef3fc7398179ff319cd510f5b5b
VonRiddarn <vonriddarn@gmail.com>
2024-12-10 03:09:57 +0100

    feat: added a utility to get a random query object based on a min and max value.

---
+++ src/utilities.ts
@@ -0,0 +1,11 @@
+const utilities = Object.freeze({
+	getRandomQuery: (min:number, max:number) => {
+
+		let a = Math.floor(Math.random() * (max - min) + min);
+		let b = Math.floor(Math.random() * (max - min) + min);
+
+		return {a: a, b: b, answer: a*b};
+	},
+});
+
+export default utilities;
