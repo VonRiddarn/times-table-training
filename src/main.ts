6122ee748f8631574dbf43c8bd707b9ecf2ddffd
VonRiddarn <vonriddarn@gmail.com>
2024-12-10 03:46:21 +0100

    feat: added first draft adjacent solutions method. drawback: creates a lot of dupes.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import utilities from "./utilities";
 const query = document.getElementById("current-query") as HTMLElement;
 
 
-let currentQuery = utilities.getRandomQuery(1, 12);
+let currentQuery = utilities.getRandomQuery(2, 5);
+let adjacentSolutions = utilities.getAdjacentSolutions(currentQuery.solution);
 
-query.innerHTML = `${currentQuery.a} x ${currentQuery.b} = ${currentQuery.solution}`;
+query.innerHTML = `${currentQuery.a} x ${currentQuery.b} = ${currentQuery.solution}<br/>`;
+
+adjacentSolutions.forEach(element => {
+	query.innerHTML += `${element}<br/>`;
+});
--- src/utilities.ts
+++ src/utilities.ts
@@ -1,4 +1,5 @@
 const utilities = Object.freeze({
+	
 	getRandomQuery: (min:number, max:number) => {
 
 		let a = Math.floor(Math.random() * (max - min) + min);
@@ -6,6 +7,15 @@ const utilities = Object.freeze({
 
 		return {a: a, b: b, solution: a*b};
 	},
+	// This is pretty, but it regularly creates duplicates.
+	getAdjacentSolutions: (solution: number, amount: number = 5) => new Array(amount).fill(0).map((e) => getAdjacentSolution(solution)),
 });
+
+const getAdjacentSolution = (solution: number): number => {
+
+	solution += Math.floor(Math.random() * (10 - 1) + 1);
+
+	return solution;
+};
 
 export default utilities;
