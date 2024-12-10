1e13c07ab056f763d6ab331c86b4083241f59af9
VonRiddarn <vonriddarn@gmail.com>
2024-12-10 04:27:38 +0100

    fix: made use of the new math query type and made sure utilities does not return dupes

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import utilities from "./utilities";
 const query = document.getElementById("current-query") as HTMLElement;
 
 
-let currentQuery = utilities.getRandomQuery(2, 5);
-let adjacentSolutions = utilities.getAdjacentSolutions(currentQuery.solution);
+let currentQuery = utilities.getRandomQuery(1, 12);
+let adjacentSolutions = utilities.getAdjacentSolutions(currentQuery);
 
 query.innerHTML = `${currentQuery.a} x ${currentQuery.b} = ${currentQuery.solution}<br/>`;
 
--- src/utilities.ts
+++ src/utilities.ts
@@ -1,19 +1,51 @@
+import MathQuery from "./math-query";
+
 const utilities = Object.freeze({
 	
-	getRandomQuery: (min:number, max:number) => {
+	getRandomQuery: (min:number, max:number): MathQuery => {
 
 		let a = Math.floor(Math.random() * (max - min) + min);
 		let b = Math.floor(Math.random() * (max - min) + min);
 
-		return {a: a, b: b, solution: a*b};
+		return {
+			a: a, 
+			b: b, 
+			solution: a*b
+		};
 	},
 	// This is pretty, but it regularly creates duplicates.
-	getAdjacentSolutions: (solution: number, amount: number = 5) => new Array(amount).fill(0).map((e) => getAdjacentSolution(solution)),
+	getAdjacentSolutions: (query: MathQuery, amount: number = 5) => {
+		const uniqueSolutions = new Set<number>();
+
+		// I feel the "mistake times (x) for add (+)" clause is so common it deserves to always be included.
+		uniqueSolutions.add(query.a + query.b);
+
+		// Emergency breakout in case of endless non-random random.
+		let currentTick = 0;
+
+		while(uniqueSolutions.size < amount && currentTick < 100)
+		{
+			const newSolution = getAdjacentSolution(query.solution);
+			if(newSolution === query.solution)
+				continue; // Wont use a tick
+
+			uniqueSolutions.add(newSolution);
+			currentTick++;
+		}
+
+		if(currentTick >= 100)
+			console.log("ERROR: Too many retries!");
+		else
+			console.log(`Succeeded in ${currentTick} ticks!`);
+
+		return [...uniqueSolutions];
+	},
 });
 
 const getAdjacentSolution = (solution: number): number => {
-
-	solution += Math.floor(Math.random() * (10 - 1) + 1);
+	console.log(solution);
+	solution += Math.floor(Math.random() * (7 - -3) + -3);
+	console.log(solution);
 
 	return solution;
 };
